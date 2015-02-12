c7a7b194f156e4d5d19a6792a54810668adc066a
Rumen G.Bogdanovski <rumen@xformcomputing.com>
2015-02-12 03:18:37 +0200

    small fix of check_config.sh

--- lin_guider/check_config.sh
+++ lin_guider/check_config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $USER == "root" ]; then
+if [ "$USER" == "root" ]; then
 	FLIST=`ls /home/*/.config/GM_software/devconf*.conf 2>/dev/null`
 else
 	FLIST=`ls ~/.config/GM_software/devconf*.conf 2>/dev/null`
 fi
 
-if [ ! $FLIST == "" ]; then
+if [ ! "x$FLIST" == "x" ]; then
 	echo "Previous configuration of Lin_guider has been detected."
 	echo "Lin_guider 3.0.0 configuration files have been changed and older configuratios may lead to erratic behaviour."
 	echo "It is highy recommended to remove the old config files (they will be renamed as *.back)"
