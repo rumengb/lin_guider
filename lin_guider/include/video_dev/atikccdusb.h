eac9377b5788e9e845c1e41c8d5c3ca0197a5e1c
Rumen G.Bogdanovski <rumen@skyarchive.org>
2015-02-22 12:44:24 +0000

    Atik: revert to atikccdsdk-0.15 because of instability

--- lin_guider/include/video_dev/atikccdusb.h
+++ lin_guider/include/video_dev/atikccdusb.h
@@ -1,7 +1,7 @@
 /*
  * ATIK CCD INDI Driver
  *
- * Copyright (c) 2013-2015 CloudMakers, s. r. o. All Rights Reserved.
+ * Copyright (c) 2013 CloudMakers, s. r. o. All Rights Reserved.
  *
  * The code is based upon Linux library source developed by
  * Jonathan Burch, Artemis CCD Ltd. It is provided by CloudMakers
@@ -18,10 +18,14 @@
 #define GUIDE_CLEAR_WE         (0x0F & ~(GUIDE_WEST | GUIDE_EAST))
 #define GUIDE_CLEAR_NS         (0x0F & ~(GUIDE_NORTH | GUIDE_SOUTH))
 
+#define QUICKER_START_EXPOSURE_DELAY  1
+#define QUICKER_READ_CCD_DELAY        2
+
 extern bool AtikDebug;
+extern bool AtikDebugOn;
 
 enum CAMERA_TYPE {
-	ORIGINAL_HSC = 1, IC24, QUICKER, IIDC, SONY_SCI
+	ORIGINAL_HSC = 1, IC24, QUICKER
 };
 
 enum COOLER_TYPE {
@@ -32,37 +36,23 @@ enum COOLING_STATE {
   COOLING_INACTIVE, COOLING_ON, COOLING_SETPOINT, WARMING_UP
 };
 
-enum PARAM_TYPE {
-  QUICKER_START_EXPOSURE_DELAY = 1, QUICKER_READ_CCD_DELAY, MAX_PACKET_SIZE
-};
-
 class AtikCamera {
 	public:
 		static int list(AtikCamera **cameras, int max);
 		virtual const char *getName() = 0;
 		virtual bool open() = 0;
 		virtual void close() = 0;
-		virtual bool setParam(PARAM_TYPE code, long value) = 0;
-		virtual bool getCapabilities(const char **name, CAMERA_TYPE *type, bool *hasShutter, bool* hasGuidePort, bool* has8BitMode, bool* hasFilterWheel, unsigned *lineCount, unsigned* pixelCountX, unsigned* pixelCountY, double* pixelSizeX, double* pixelSizeY, unsigned* maxBinX, unsigned* maxBinY, unsigned *tempSensorCount, COOLER_TYPE* cooler) = 0;
-		virtual bool getTemperatureSensorStatus(unsigned sensor, float *currentTemp) = 0;
-		virtual bool getCoolingStatus(COOLING_STATE *state, float* targetTemp, float *power) = 0;
-		virtual bool setCooling(float targetTemp) = 0;
+		virtual bool setParam(int code, long value) = 0;
+		virtual bool getCapabilities(const char **name, CAMERA_TYPE *type, bool *hasShutter, bool* hasGuidePort, unsigned* pixelCountX, unsigned* pixelCountY, double* pixelSizeX, double* pixelSizeY, unsigned* maxBinX, unsigned* maxBinY, COOLER_TYPE* cooler) = 0;
 		virtual bool initiateWarmUp() = 0;
-		virtual bool getFilterWheelStatus(unsigned *filterCount, bool *moving, unsigned *current, unsigned *target) = 0;
-		virtual bool setFilter(unsigned index) = 0;
+		virtual bool getCoolerState(COOLING_STATE *state, float* targetTemp, float* currentTemp) = 0;
+		virtual bool setCooling(float targetTemp) = 0;
 		virtual bool startExposure(bool amp) = 0;
 		virtual bool abortExposure() = 0;
 		virtual bool readCCD(unsigned startX, unsigned startY, unsigned sizeX, unsigned sizeY, unsigned binX, unsigned binY) = 0;
-		virtual bool readCCD(unsigned startX, unsigned startY, unsigned sizeX, unsigned sizeY, unsigned binX, unsigned binY, double delay) = 0;
 		virtual bool getImage(unsigned short* imgBuf, unsigned imgSize) = 0;
 		virtual bool setShutter(bool open) = 0;
-		virtual bool setGuideRelays(unsigned short mask) = 0;
-		virtual bool setGPIODirection(unsigned short mask) = 0;
-		virtual bool getGPIO(unsigned short *mask) = 0;
-		virtual bool setGPIO(unsigned short mask) = 0;
-		virtual unsigned delay(double delay) = 0;
-    virtual unsigned imageWidth(unsigned width, unsigned binX) = 0;
-    virtual unsigned imageHeight(unsigned height, unsigned binY) = 0;
+		virtual bool setGuideRelays(short mask) = 0;
     virtual ~AtikCamera() { };
 };
 
