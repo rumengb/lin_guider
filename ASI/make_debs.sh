9c659ef89fc6427158bd8c1a32f524ce1fe27126
Rumen G.Bogdanovski <rumen@xformcomputing.com>
2015-02-22 14:53:13 +1300

    ASI: Add deb create scripts for the SDK

---
+++ ASI/make_debs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+PACKAGE="asiccdsdk"
+VERSION="0.1.0214"
+
+# arch i386
+ARCH="i386"
+NAME="$PACKAGE-$VERSION-$ARCH"
+
+install -d $NAME/DEBIAN
+install -m 644 control $NAME/DEBIAN
+sed "s/VERSION/$VERSION/g" -i $NAME/DEBIAN/control
+sed "s/ARCH/$ARCH/g" -i $NAME/DEBIAN/control
+install -d $NAME/etc/udev/rules.d
+install -m 644 99-asi.rules $NAME/etc/udev/rules.d
+install -d $NAME/usr/lib
+cp lib/x86/* $NAME/usr/lib
+install -d $NAME/usr/include
+install -m 644 include/* $NAME/usr/include
+
+dpkg -b $NAME $NAME.deb
+rm -r $NAME
+
+# arch amd64
+ARCH="amd64"
+NAME="$PACKAGE-$VERSION-$ARCH"
+
+install -d $NAME/DEBIAN
+install -m 644 control $NAME/DEBIAN
+sed "s/VERSION/$VERSION/g" -i $NAME/DEBIAN/control
+sed "s/ARCH/$ARCH/g" -i $NAME/DEBIAN/control
+install -d $NAME/etc/udev/rules.d
+install -m 644 99-asi.rules $NAME/etc/udev/rules.d
+install -d $NAME/usr/lib
+cp lib/x64/* $NAME/usr/lib
+install -d $NAME/usr/include
+install -m 644 include/* $NAME/usr/include
+
+dpkg -b $NAME $NAME.deb
+rm -r $NAME
+
+# arch armhf
+ARCH="armhf"
+NAME="$PACKAGE-$VERSION-$ARCH"
+
+install -d $NAME/DEBIAN
+install -m 644 control $NAME/DEBIAN
+sed "s/VERSION/$VERSION/g" -i $NAME/DEBIAN/control
+sed "s/ARCH/$ARCH/g" -i $NAME/DEBIAN/control
+install -d $NAME/etc/udev/rules.d
+install -m 644 99-asi.rules $NAME/etc/udev/rules.d
+install -d $NAME/usr/lib
+cp lib/armhf/* $NAME/usr/lib
+install -d $NAME/usr/include
+install -m 644 include/* $NAME/usr/include
+
+dpkg -b $NAME $NAME.deb
+rm -r $NAME
+
+
+
+
