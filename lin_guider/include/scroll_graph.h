6a12282b32ec93cfd9000a4280608a485cd1959b
Rumen G.Bogdanovski <rumen@skyarchive.org>
2016-01-25 00:52:42 +0200

    Andrew's incomplete work on guider and graph API

--- lin_guider/include/scroll_graph.h
+++ lin_guider/include/scroll_graph.h
@@ -29,66 +29,64 @@
 #include <QPainter>
 
 
-#define RA_LINE    0
-#define DEC_LINE   1
-
-typedef struct
-{
-	double *line[2];
-}delta_data_t;
-
-
-
 class cscroll_graph
 {
 public:
-	cscroll_graph( QWidget *own, int client_width, int client_height, int cell_nx, int cell_ny );
+	cscroll_graph( int client_width, int client_height, int cell_nx, int cell_ny );
 	virtual ~cscroll_graph();
 	
 	QImage *get_buffer( void ) const;
-	bool add_point( double ra, double dec );
+	void add_point( double ra, double dec );
 	void set_visible_ranges( int rx, int ry );
 	void get_visible_ranges( int *rx, int *ry ) const;
 	int  get_gridx_N( void ) const;
 	int  get_gridy_N( void ) const;
-	void reset_view( void );
 	void reset_data( void );
 	void on_paint( void );
 	void get_screen_size( int *sx, int *sy ) const;
 	
-private:
+protected:
+	#define RA_LINE    0
+	#define DEC_LINE   1
+
+	typedef struct
+	{
+		double *line[2];
+	}delta_data_t;
+
 	// view
-	QWidget *owner;
-	QImage *buffer;
-	QPainter canvas;
-	QColor BKGD_COLOR, RA_COLOR, DEC_COLOR, GRID_COLOR, WHITE_COLOR, GRID_FONT_COLOR;
-	QPen pen;	
-	QBrush brush;
-	int half_buffer_size_wd;
-	int half_buffer_size_ht;
-	int client_rect_wd;
-	int client_rect_ht;
-	bool need_refresh;
+	QPainter m_canvas;
+	QPen     m_pen;
+	QBrush   m_brush;
+	QColor   BKGD_COLOR, RA_COLOR, DEC_COLOR, GRID_COLOR, WHITE_COLOR, GRID_FONT_COLOR;
+	int  m_half_buffer_size_wd;
+	int  m_half_buffer_size_ht;
+	int  m_client_rect_wd;
+	int  m_client_rect_ht;
+	bool m_need_refresh;
 	
 	// data
-	delta_data_t data;
-	int data_cnt;
-	int	data_idx;
-	int gridx_N;
-	int gridy_N;
+	delta_data_t m_data;
+	int m_data_cnt;
+	int	m_data_idx;
+	int m_gridx_N;
+	int m_gridy_N;
 	
 	// grid vars...
-	double grid_step_x, grid_step_y, grid_view_step_x, grid_view_step_y;
-	int font_ht_k;
+	double m_grid_step_x, m_grid_step_y, m_grid_view_step_x, m_grid_view_step_y;
+	int m_font_ht_k;
 	
 	// control
-	int vis_range_x, vis_range_y;
-	int half_vis_range_x, half_vis_range_y;
-	
-	void refresh( void );
-	void draw_grid( double kx, double ky );
-	void init_render_vars( void );
-	
+	int m_vis_range_x, m_vis_range_y;
+	int m_half_vis_range_x, m_half_vis_range_y;
+
+	// may be overloaded in inherited classes
+	virtual void refresh( void );
+	virtual void draw_grid( double kx, double ky );
+	virtual void init_render_vars( void );
+
+private:
+	QImage  *m_buffer;
 };
 
 #endif /*CSCROLL_GRAPH_H_*/
