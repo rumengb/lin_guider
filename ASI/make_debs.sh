7c64e3ddffac185172f4cbda30287af12069dd9c
Rumen G.Bogdanovski <rumen@skyarchive.org>
2016-09-12 19:04:47 +0300

    Use latest ASI SDK v.0.3.0908

--- ASI/make_debs.sh
+++ ASI/make_debs.sh
@@ -6,7 +6,7 @@ if [ "$USER" != "root" ]; then
 fi
 
 PACKAGE="libasicamera"
-VERSION="0.3.0623"
+VERSION="0.3.0908"
 
 # arch i386
 ARCH="i386"
@@ -46,8 +46,8 @@ chown -R root:root $NAME
 dpkg -b $NAME $NAME.deb
 rm -r $NAME
 
-# arch armhf
-ARCH="armhf"
+# arch armhf v6
+ARCH="armhf-v6"
 NAME="$PACKAGE-$VERSION-$ARCH"
 
 install -d $NAME/DEBIAN
@@ -64,3 +64,42 @@ install -m 644 include/* $NAME/usr/include
 chown -R root:root $NAME
 dpkg -b $NAME $NAME.deb
 rm -r $NAME
+
+# arch armhf v7
+ARCH="armhf-v7"
+NAME="$PACKAGE-$VERSION-$ARCH"
+
+install -d $NAME/DEBIAN
+install -m 644 control $NAME/DEBIAN
+sed "s/VERSION/$VERSION/g" -i $NAME/DEBIAN/control
+sed "s/ARCH/$ARCH/g" -i $NAME/DEBIAN/control
+install -d $NAME/etc/udev/rules.d
+install -m 644 99-asi.rules $NAME/etc/udev/rules.d
+install -d $NAME/usr/lib
+cp -a lib/armv7/* $NAME/usr/lib
+install -d $NAME/usr/include
+install -m 644 include/* $NAME/usr/include
+
+chown -R root:root $NAME
+dpkg -b $NAME $NAME.deb
+rm -r $NAME
+
+# arch armhf v8
+ARCH="armhf-v8"
+NAME="$PACKAGE-$VERSION-$ARCH"
+
+install -d $NAME/DEBIAN
+install -m 644 control $NAME/DEBIAN
+sed "s/VERSION/$VERSION/g" -i $NAME/DEBIAN/control
+sed "s/ARCH/$ARCH/g" -i $NAME/DEBIAN/control
+install -d $NAME/etc/udev/rules.d
+install -m 644 99-asi.rules $NAME/etc/udev/rules.d
+install -d $NAME/usr/lib
+cp -a lib/armv8/* $NAME/usr/lib
+install -d $NAME/usr/include
+install -m 644 include/* $NAME/usr/include
+
+chown -R root:root $NAME
+dpkg -b $NAME $NAME.deb
+rm -r $NAME
+
